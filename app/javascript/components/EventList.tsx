ca22a542109eb3442a3b8d1974edea90b7ea55c3
miura <s.miura@mof-mof.co.jp>
2022-08-15 22:48:20 +0900

    バグ修正

--- app/javascript/components/EventList.tsx
+++ app/javascript/components/EventList.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-interface EventListProps {
-  events: Event[]
-  id: number
-  event_type: String
-  event_date: Number
+type Prop = {
+    id: number,
+    event_type: string,
+    event_date: string,
+    title: string,
+    speaker: string,
+    host: string,
+    published: boolean,
+}
+type Event = {
+  events :Prop[]
 }
 
-const EventList: React.FC<EventListProps> = ({ events }) => {
-  const renderEvents = (eventArray :any) => {
-    eventArray = eventArray.sort((a:any, b:any) => new Date, Number(b.event_date) - new Date(a.event_date));
-
-    return eventArray.map((event: any) => (
+const EventList:React.FC<Event>  = ({ events }) => {
+  const renderEvents = (eventArray: Prop[]) => {
+    eventArray.sort((a: Prop, b: Prop) => {
+      const aEventDate = new Date(a.event_date)
+      const bEventDate = new Date(b.event_date)
+      return aEventDate < bEventDate ? 1 : -1;
+    });
+    return eventArray.map((event) => (
       <li key={event.id}>
         {event.event_date}
         {' - '}
